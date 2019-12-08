a95d99bcc5d235d19338dea1f9e1b06085247cfb
Jay Wen <jaywen89@gmail.com>
2019-12-08 23:29:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,7 @@ def player_numbers(team_name)
          data.each do |player|
            if [:player_name] == players_name
              new_hash = player.delete_if do |k,v|
-               k == 
+               k == :player_name
           end
          end
       end
