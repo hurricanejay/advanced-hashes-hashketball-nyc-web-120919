fba44fa5cc6248a5c1d442b8e1ddd280813aeba7
Jay Wen <jaywen89@gmail.com>
2019-12-08 23:02:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,7 +158,13 @@ def team_names
   end
 end
 
-
+def players_numbers(team_name)
+  game_hash.map do |place, team|
+    if team[:players] == players
+      return team[:number]
+    end
+  end
+end
 
 
 
